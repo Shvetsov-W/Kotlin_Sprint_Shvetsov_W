d5493324846d6c82bbbab60c1b6c597a579945dd
victor <v89617755598@yandex.ru>
2023-07-22 14:13:27 +0500

    lesson3-task1 created a greeting output method

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -1,1 +1,20 @@
 package lesson_3
+
+fun main() {
+    val name: String = "Victor"
+    var greeting: String = outputGreeting(name, TimeOfDay.DAY)
+    println(greeting)
+    greeting = outputGreeting(name, TimeOfDay.NIGHT)
+    println(greeting)
+}
+
+fun outputGreeting(name: String, timeOfDay: TimeOfDay): String {
+    return when (timeOfDay) {
+        TimeOfDay.DAY -> "Доброго дня $name!"
+        TimeOfDay.NIGHT -> "Доброй ночи $name!"
+    }
+}
+
+enum class TimeOfDay {
+    DAY, NIGHT
+}
