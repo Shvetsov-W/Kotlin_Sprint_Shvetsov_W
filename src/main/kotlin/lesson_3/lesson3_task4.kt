45ba29dd3aba0832701bbf3404d42b0dab8bff9e
victor <v89617755598@yandex.ru>
2023-07-22 14:17:42 +0500

    lesson3-task4 created the output of chess moves

--- src/main/kotlin/lesson_3/lesson3_task4.kt
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -1,1 +1,25 @@
 package lesson_3
+
+fun main() {
+    var start: String = "E${2}"
+    var finish: String = "E${4}"
+    var movesNumber: Int = 1
+
+    var str: String = "$start-$finish;$movesNumber"
+    println(str)
+
+    start = "G${2}"
+    finish = "G${4}"
+    movesNumber = 2
+
+    str = "$start-$finish;$movesNumber"
+    println(str)
+
+    start = "D${2}"
+    finish = "D${2 + 1}"
+    movesNumber = 3
+
+    str = "$start-$finish;$movesNumber"
+    println(str)
+
+}
