7912dec992a2e26076be6bc0425dd7e6d9d57f92
victor <v89617755598@yandex.ru>
2023-07-22 14:16:04 +0500

    lesson3-task3 Created a multiplication table

--- src/main/kotlin/lesson_3/lesson3_task3.kt
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -1,1 +1,19 @@
 package lesson_3
+
+fun main() {
+    val number: Int = 5
+
+    println(
+        """$number*1=${number * 1}
+        |$number*2=${number * 2}
+        |$number*3=${number * 3}
+        |$number*4=${number * 4}
+        |$number*5=${number * 5}
+        |$number*6=${number * 6}
+        |$number*7=${number * 7}
+        |$number*8=${number * 8}
+        |$number*9=${number * 9}
+        |$number*10=${number * 10}
+    """.trimMargin()
+    )
+}
