2b76bdcaa411252d18d7ec45be42d5db2ac2ef20
victor <v89617755598@yandex.ru>
2023-07-22 14:20:45 +0500

    lesson3-task5 created a method for parsing a string

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -1,1 +1,19 @@
 package lesson_3
+
+fun main() {
+    val serverData: String = "D2-D4;0"
+    val (start, finish, movesNumber) = readData(serverData)
+    println(
+        """start = $start
+        |finish = $finish
+        |movesNumber = $movesNumber
+    """.trimMargin()
+    )
+}
+
+data class ChessData(val start: String, val finish: String, val moveNumber: Int)
+
+fun readData(data: String): ChessData {
+    val charData: CharArray = data.toCharArray()
+    return ChessData("${charData[0]}${charData[1]}", "${charData[3]}${charData[4]}", charData[6].digitToInt())
+}
