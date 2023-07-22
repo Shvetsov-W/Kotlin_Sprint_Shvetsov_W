26d2101b1ed09cbd0477c5a5997e637732b4ac94
victor <v89617755598@yandex.ru>
2023-07-22 14:06:11 +0500

    lesson2-task4 created a method for calculating the bonus

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,1 +1,13 @@
 package lesson_2
+
+fun main() {
+    val crystals: Int = 7
+    val ferrum: Int = 11
+    println("Бонус кристаллической руды: ${calculateBonus(20, crystals)}")
+    println("Бонус железной руды: ${calculateBonus(20, ferrum)}")
+
+}
+
+fun calculateBonus(percent: Int, material: Int): Int {
+    return (material.toDouble() / 100 * percent).toInt()
+}
