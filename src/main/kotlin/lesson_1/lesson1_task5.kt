5262516495faa8fa6658bd32e8c703ad343289a7
victor <v89617755598@yandex.ru>
2023-07-22 13:52:41 +0500

    lesson1-task5 converted seconds into minutes and hours, created methods for this

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,1 +1,11 @@
 package lesson_1
+
+fun main() {
+    val totalSeconds: Int = 6480
+    val hours: Int = totalSeconds / 3600
+    val minutes: Int = (totalSeconds % 3600) / 60
+    val seconds: Int = totalSeconds % 60
+
+    println(String.format("Время проведенное в космосе: %02d:%02d:%02d ", hours, minutes, seconds))
+
+}
