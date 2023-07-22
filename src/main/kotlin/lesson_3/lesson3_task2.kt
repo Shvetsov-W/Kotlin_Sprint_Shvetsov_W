f372f6029805f0fa2db1cf468217e0a3ca70a306
victor <v89617755598@yandex.ru>
2023-07-22 14:14:48 +0500

    lesson3-task2 created output of different last names

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -1,1 +1,13 @@
 package lesson_3
+
+fun main() {
+    var lastName: String = "Андреева"
+    val firstName: String = "Татьяна"
+    val patronymic: String = "Сергеевна"
+    var age: Int = 20
+
+    println("$lastName $firstName $patronymic, $age")
+    age = 22
+    lastName = "Сидорова"
+    println("$lastName $firstName $patronymic, $age")
+}
