5203352cf6dfe427ad3f0fb021093fa0e814936a
victor <v89617755598@yandex.ru>
2023-07-22 13:59:51 +0500

    lesson2-task2 Calculation of salaries and expenses

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,1 +1,19 @@
 package lesson_2
+
+fun main() {
+    val permanentEmployees: Int = 50
+    val interns: Int = 30
+    val salaryPerEmployees: Int = 50000
+    val salaryInterns: Int = 30000
+    var sumSalaryPerEmployees: Int = permanentEmployees * salaryPerEmployees
+    val sumSalaryInterns: Int = interns * salaryInterns
+    val generalExpenses: Int = sumSalaryPerEmployees + sumSalaryInterns
+    val averageSalary: Int = generalExpenses / (permanentEmployees + interns)
+
+    println(
+        """Расходы на выплату зарплаты постоянных сотрудников: $sumSalaryPerEmployees
+        |Общие расходы по ЗП после прихода стажеров: $generalExpenses
+        |Средняя ЗП одного сотрудника после устройства стажеров: $averageSalary
+    """.trimMargin()
+    )
+}
