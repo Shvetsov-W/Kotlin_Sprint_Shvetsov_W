1e7b01ff9b8f3f0bcd553d60915334e0d94ab10a
victor <v89617755598@yandex.ru>
2023-07-22 13:54:47 +0500

    lesson2-task1 Calculating the arithmetic mean

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,1 +1,13 @@
 package lesson_2
+
+fun main() {
+    val grades = listOf<Int>(3, 4, 3, 5)
+    println("Средняя оценка: %.2f".format(grades.mean()))
+}
+
+fun List<Int>.mean(): Double {
+    var sum: Int = 0
+    for (element in this) sum += element
+    return (sum.toDouble() / this.size)
+
+}
