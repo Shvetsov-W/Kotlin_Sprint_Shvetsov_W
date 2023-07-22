5d2a6d0fefea4c215226e9b055dc1085ee9b07de
victor <v89617755598@yandex.ru>
2023-07-22 13:48:05 +0500

    lesson1-task2 output to console

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,1 +1,13 @@
 package lesson_1
+
+fun main(args: Array<String>) {
+    println("Количество заказов: $orderCounter")
+    println(thanksForBuy)
+    // println(numberOfEmployees)
+    numberOfEmployees--  //1 работник уволился
+    println("Количество работников: $numberOfEmployees")
+}
+
+var orderCounter: Int = 75
+val thanksForBuy: String = "THANKS FOR PURCHASE - COME YET!"
+var numberOfEmployees: Int = 2000
