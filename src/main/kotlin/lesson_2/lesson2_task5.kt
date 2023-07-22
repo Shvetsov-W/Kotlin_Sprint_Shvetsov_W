2089637fa44ea90d9dacc912dd02b1cf4f2ee352
victor <v89617755598@yandex.ru>
2023-07-22 14:10:12 +0500

    lesson2-task5 created a compound Interest calculation method

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,1 +1,16 @@
 package lesson_2
+
+import java.util.*
+import kotlin.math.pow
+
+fun main() {
+    val initialSum: Int = 70_000
+    val interestRate: Double = 16.7
+    val years: Int = 20
+    val result: Double = calculateInterest(initialSum, interestRate, 20)
+    println(String.format(Locale.ROOT, "Размер вклада через 20 лет: %.3f", result))
+
+}
+
+fun calculateInterest(initialSum: Int, percent: Double, years: Int): Double =
+    initialSum * (1 + percent / 100).pow(years)
