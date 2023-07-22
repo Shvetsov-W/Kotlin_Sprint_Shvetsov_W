9cedb225bca95b48ef95552d82d3e3470b5d29fc
victor <v89617755598@yandex.ru>
2023-07-22 13:49:09 +0500

    lesson1-task3 Created variables and printed date and time

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,1 +1,18 @@
 package lesson_1
+
+fun main() {
+    val year: String = "1961"
+    var hour: String = "09"
+    var minute: String = "07"
+
+    println(
+        """Год: $year
+        |Час: $hour
+        |Минута: $minute""".trimMargin()
+    )
+
+    hour = "10"
+    minute = "55"
+
+    print("Время посадки: $hour:$minute")
+}
